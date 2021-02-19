89b02155bc65b59ca4ff68d33f8676eb629dffd7
Shiftgithub <60180521+Shiftgithub@users.noreply.github.com>
2021-02-20 00:17:48 +0600

    tryexcept

---
+++ c1/w2/tryexcept.py
@@ -0,0 +1,17 @@
+str = input('Write Anything: ')
+
+try:
+    str = int(str)
+except:
+    str = -1
+
+print('First ', str)
+
+inte = input('Write Anything Integer: ')
+
+try:
+    inte = int(inte)
+except:
+    inte = -1
+
+print('Second ', inte)
