d84555dd0d1fb5cf73542bb36e79d21c893aa513
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-03-26 23:28:53 +0600

    8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
    You can download the sample data at http://www.py4e.com/code3/romeo.txt

--- Python Data Structures/Week4/Assignment-8.4/code.py
+++ Python Data Structures/Week4/Assignment-8.4/code.py
@@ -2,7 +2,7 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-    str =line.strip().split()
+    str = line.strip().split()
     for single in str:
         if single in lst:
             continue
