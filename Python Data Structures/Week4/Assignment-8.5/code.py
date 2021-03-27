d9ce505ffa98d9fb9ff5ed3d440d5fd6a476c1ce
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-03-27 23:33:40 +0600

    8.5 Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
    From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
    
    You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
    
    Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count.
    
    You can download the sample data at http://www.py4e.com/code3/mbox-short.txt

--- Python Data Structures/Week4/Assignment-8.5/code.py
+++ Python Data Structures/Week4/Assignment-8.5/code.py
@@ -3,10 +3,16 @@ if len(fname) < 1:
     fname = "mbox-short.txt"
 
 fh = open(fname)
+count = 0
 
 for line in fh:
-    string = line.rstrip()
-    
-count = 0
+    line = line.rstrip()
+    if not line.startswith('From'):
+        continue
+    if line.startswith('From:'):
+        continue
+    elements = line.split()
+    print(elements[1])
+    count = count + 1
 
 print("There were", count, "lines in the file with From as the first word")
