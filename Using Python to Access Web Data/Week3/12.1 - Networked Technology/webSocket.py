f12ad232a0559eefd07ff9247e76ebbd04bb67d2
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-04-08 21:35:28 +0600

    socket connection

--- Using Python to Access Web Data/Week3/12.1 - Networked Technology/webSocket.py
+++ Using Python to Access Web Data/Week3/12.1 - Networked Technology/webSocket.py
@@ -2,7 +2,7 @@ import socket
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.text HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 
 mySocket.send(cmd)
 
