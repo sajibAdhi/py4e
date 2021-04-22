83261e13124805f3803a6372d07aca701789f5e0
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-04-22 22:41:19 +0600

    xml parsing

---
+++ Using Python to Access Web Data/Week5/Parsing XML/xml1.py
@@ -0,0 +1,16 @@
+import xml.etree.ElementTree as ET
+data = '''
+<person>
+    <name>Sajib Adhikary</name>
+    <phone type="intl">
+        +8801000000000
+    </phone>
+    <email hide="yes" />
+</person>
+'''
+
+xmlTree = ET.fromstring(data)
+
+print("Person Name:", xmlTree.find('name').text)
+print("Person Phone:", xmlTree.find('phone').text)
+print("Person Email:", xmlTree.find('email').text)
