fe4ced1a320c7badc4284a9d45f751f62b21344e
sajibAdhi <60180521+Shiftgithub@users.noreply.github.com>
2021-08-05 17:12:06 +0600

    Many Students in Many Courses

--- Using Database With Python/Week4/Many Students in Many Courses/roster.py
+++ Using Database With Python/Week4/Many Students in Many Courses/roster.py
@@ -6,31 +6,31 @@ cur = conn.cursor()
 
 # Do some setup
 cur.executescript('''
-    DROP TABLE IF EXISTS User;
-    DROP TABLE IF EXISTS Member;
-    DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS User;
+DROP TABLE IF EXISTS Member;
+DROP TABLE IF EXISTS Course;
 
-    CREATE TABLE User (
-        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        name   TEXT UNIQUE
-    );
+CREATE TABLE User (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name   TEXT UNIQUE
+);
 
-    CREATE TABLE Course (
-        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-        title  TEXT UNIQUE
-    );
+CREATE TABLE Course (
+    id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title  TEXT UNIQUE
+);
 
-    CREATE TABLE Member (
-        user_id     INTEGER,
-        course_id   INTEGER,
-        role        INTEGER,
-        PRIMARY KEY (user_id, course_id)
-    )
+CREATE TABLE Member (
+    user_id     INTEGER,
+    course_id   INTEGER,
+    role        INTEGER,
+    PRIMARY KEY (user_id, course_id)
+)
 ''')
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -43,8 +43,9 @@ for entry in json_data:
 
     name = entry[0]
     title = entry[1]
+    role = entry[2]
 
-    print((name, title))
+    print((name, title,role))
 
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
@@ -57,7 +58,7 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
 
     conn.commit()
